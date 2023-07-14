b1a87122fb88f668e73225b2082c169b08ef345f
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-07-14 11:47:47 +0800

    chore: update

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,7 +2,7 @@ import { elementUi2, elementUi2Components } from './elementUI'
 import { antd4, antd4Components, antd5, antd5Components } from './antd'
 import { elementPlus2, elementPlus2Components } from './elementPlus'
 import { vant4, vant4Components } from './vant'
-import { varlet2, varlet2Components } from './varlet'
+// import { varlet2, varlet2Components } from './varlet'
 
 export default {
   elementUi2,
@@ -10,11 +10,11 @@ export default {
   antd5,
   elementPlus2,
   vant4,
-  varlet2,
+  // varlet2,
   elementUi2Components,
   antd4Components,
   antd5Components,
   elementPlus2Components,
   vant4Components,
-  varlet2Components,
+  // varlet2Components,
 }
