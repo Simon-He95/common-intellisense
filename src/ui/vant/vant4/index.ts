9b8787dad97cad2952d4746b738d48969386aa67
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-07-17 21:56:21 +0800

    chore(van): add cell cellGroup

--- src/ui/vant/vant4/index.ts
+++ src/ui/vant/vant4/index.ts
@@ -13,6 +13,8 @@ import button from './button.json'
 import calendar from './calendar.json'
 import card from './card.json'
 import cascader from './cascader.json'
+import cell from './cell.json'
+import cellGroup from './cellGroup.json'
 
 export function vant4() {
   const map: any = [
@@ -30,6 +32,8 @@ export function vant4() {
     calendar,
     card,
     cascader,
+    cell,
+    cellGroup,
   ]
 
   return propsReducer(map)
@@ -50,6 +54,8 @@ export function vant4Components() {
     ['van-calendar', '日历 日历组件用于选择日期或日期区间。'],
     ['van-card', '卡片 商品卡片，用于展示商品的图片、价格等信息。'],
     ['van-cascader', '级联选择 级联选择框，用于多层级数据的选择，典型场景为省市区选择。'],
+    ['van-cell', '单元格 单元格为列表中的单个展示项。'],
+    ['van-cell-group', '单元格组 单元格为列表中的单个展示项。'],
   ]
   return componentsReducer(map)
 }
