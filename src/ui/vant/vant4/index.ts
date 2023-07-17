144ffbc9b8d239d711f98ceb47f3d8b998c7f176
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-07-17 17:15:07 +0800

    chore(van): add van-address-edit

--- src/ui/vant/vant4/index.ts
+++ src/ui/vant/vant4/index.ts
@@ -3,6 +3,7 @@ import actionBar from './actionBar.json'
 import actionBarButton from './actionBarButton.json'
 import actionBarIcon from './actionBarIcon.json'
 import actionSheet from './actionSheet.json'
+import addressEdit from './addressEdit.json'
 
 export function vant4() {
   const map: any = [
@@ -10,6 +11,7 @@ export function vant4() {
     actionBarButton,
     actionBarIcon,
     actionSheet,
+    addressEdit,
   ]
 
   return propsReducer(map)
@@ -21,6 +23,7 @@ export function vant4Components() {
     ['van-action-bar-icon', '徽标提示 在 ActionBarIcon 组件上设置 dot 属性后，会在图标右上角展示一个小红点；设置 badge 属性后，会在图标右上角展示相应的徽标'],
     ['van-action-bar-button', '自定义按钮颜色 通过 ActionBarButton 的 color 属性可以自定义按钮的颜色，支持传入 linear-gradient 渐变色。'],
     ['van-action-sheet', '动作面板 底部弹起的模态面板，包含与当前情境相关的多个选项。'],
+    ['van-address-edit', '地址编辑 展示地址信息列表。'],
   ]
   return componentsReducer(map)
 }
