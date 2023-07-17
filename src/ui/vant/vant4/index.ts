a241259a69d9fcc5c3e9bc14cf206f7dd6b2ec08
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-07-17 17:53:15 +0800

    chore(van): add area

--- src/ui/vant/vant4/index.ts
+++ src/ui/vant/vant4/index.ts
@@ -5,6 +5,7 @@ import actionBarIcon from './actionBarIcon.json'
 import actionSheet from './actionSheet.json'
 import addressEdit from './addressEdit.json'
 import addressList from './addressList.json'
+import area from './area.json'
 
 export function vant4() {
   const map: any = [
@@ -14,6 +15,7 @@ export function vant4() {
     actionSheet,
     addressEdit,
     addressList,
+    area,
   ]
 
   return propsReducer(map)
@@ -26,6 +28,7 @@ export function vant4Components() {
     ['van-action-bar-button', '自定义按钮颜色 通过 ActionBarButton 的 color 属性可以自定义按钮的颜色，支持传入 linear-gradient 渐变色。'],
     ['van-action-sheet', '动作面板 底部弹起的模态面板，包含与当前情境相关的多个选项。'],
     ['van-address-edit', '地址编辑 展示地址信息列表。'],
+    ['van-area', '省市区选择 省市区三级联动选择，通常与弹出层组件配合使用。'],
   ]
   return componentsReducer(map)
 }
