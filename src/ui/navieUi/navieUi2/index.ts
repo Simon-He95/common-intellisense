cbd6e987516ed7e73ed48259feab4aec2ec42019
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-07-21 13:40:02 +0800

    chore(navieUi): add badge

--- src/ui/navieUi/navieUi2/index.ts
+++ src/ui/navieUi/navieUi2/index.ts
@@ -1,11 +1,13 @@
 import { componentsReducer, propsReducer } from '../../utils'
 import avatar from './avatar.json'
 import avatarGroup from './avatarGroup.json'
+import badge from './badge.json'
 
 export function navieUi2(extensionContext: any) {
   const map: any = [
     avatar,
     avatarGroup,
+    badge,
   ]
 
   return propsReducer(map)
@@ -19,6 +21,9 @@ export function navieUi2Components() {
   src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"
 />`],
     ['n-avatar-group', '头像组', '<n-avatar-group :options="options" :size="40" :max="3" />'],
+    ['n-badge', '标记', `<n-badge dot type="error">
+    <n-avatar />
+  </n-badge>`],
   ]
   return componentsReducer(map)
 }
