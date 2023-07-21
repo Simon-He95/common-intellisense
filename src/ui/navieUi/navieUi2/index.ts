887d0bf249b43cfae29a8dd6230d0bc642ef6319
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-07-21 13:37:49 +0800

    chore(navieUi): add avatar avatarGroup

--- src/ui/navieUi/navieUi2/index.ts
+++ src/ui/navieUi/navieUi2/index.ts
@@ -1,8 +1,11 @@
 import { componentsReducer, propsReducer } from '../../utils'
+import avatar from './avatar.json'
+import avatarGroup from './avatarGroup.json'
 
 export function navieUi2(extensionContext: any) {
   const map: any = [
-
+    avatar,
+    avatarGroup,
   ]
 
   return propsReducer(map)
@@ -10,11 +13,12 @@ export function navieUi2(extensionContext: any) {
 
 export function navieUi2Components() {
   const map = [
-    ['n-avatar', '头像', ` <n-avatar
+    ['n-avatar', '头像', `<n-avatar
   round
   size="small"
   src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"
 />`],
+    ['n-avatar-group', '头像组', '<n-avatar-group :options="options" :size="40" :max="3" />'],
   ]
   return componentsReducer(map)
 }
