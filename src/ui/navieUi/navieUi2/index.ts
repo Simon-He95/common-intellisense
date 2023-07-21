899f02b6b99460d39031364c2d4e497f70c4dad3
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-07-21 15:05:33 +0800

    chore(navieUi): add button buttonGroup breadcrumb breadcrumbItem

--- src/ui/navieUi/navieUi2/index.ts
+++ src/ui/navieUi/navieUi2/index.ts
@@ -1,13 +1,21 @@
 import { componentsReducer, propsReducer } from '../../utils'
+import button from './button.json'
+import buttonGroup from './buttonGroup.json'
 import avatar from './avatar.json'
 import avatarGroup from './avatarGroup.json'
 import badge from './badge.json'
+import breadcrumb from './breadcrumb.json'
+import breadcrumbItem from './breadcrumbItem.json'
 
 export function navieUi2(extensionContext: any) {
   const map: any = [
     avatar,
     avatarGroup,
     badge,
+    breadcrumb,
+    breadcrumbItem,
+    button,
+    buttonGroup,
   ]
 
   return propsReducer(map)
@@ -24,6 +32,24 @@ export function navieUi2Components() {
     ['n-badge', '标记', `<n-badge dot type="error">
     <n-avatar />
   </n-badge>`],
+    ['n-breadcrumb', '面包屑', `<n-breadcrumb>
+  <n-breadcrumb-item>
+    <n-icon :component="MdCash" /> 北京总行</n-breadcrumb-item>
+  <n-breadcrumb-item>
+    <n-icon :component="MdCash" /> 天津分行</n-breadcrumb-item>
+  <n-breadcrumb-item>
+    <n-icon :component="MdCash" /> 平山道支行</n-breadcrumb-item>
+</n-breadcrumb>`],
+    ['n-breadcrumb-item', '面包屑', `<n-breadcrumb>
+<n-breadcrumb-item>
+  <n-icon :component="MdCash" /> 北京总行</n-breadcrumb-item>
+<n-breadcrumb-item>
+  <n-icon :component="MdCash" /> 天津分行</n-breadcrumb-item>
+<n-breadcrumb-item>
+  <n-icon :component="MdCash" /> 平山道支行</n-breadcrumb-item>
+</n-breadcrumb>`],
+    ['n-button', '按钮', '<n-button>Default</n-button>'],
+    ['n-button-group', '按钮组', ' <n-button-group vertical><n-button>Default</n-button></n-button-group>'],
   ]
   return componentsReducer(map)
 }
