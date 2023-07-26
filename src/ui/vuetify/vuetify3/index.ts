32e435ed4c867785935e1fb5b50bb762cd453b3e
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-07-26 11:46:05 +0800

    chore(vuetify): add card

--- src/ui/vuetify/vuetify3/index.ts
+++ src/ui/vuetify/vuetify3/index.ts
@@ -1,9 +1,11 @@
 import { componentsReducer, propsReducer } from '../../utils'
 import button from './button.json'
+import card from './card.json'
 
 export function vuetify3() {
   const map: any = [
     button,
+    card,
   ]
 
   return propsReducer(map)
@@ -12,6 +14,7 @@ export function vuetify3() {
 export function vuetify3Components() {
   const map: any[] = [
     ['v-button', '按钮', '<v-btn>Button</v-btn>'],
+    ['v-card', '卡片', '<v-card text="..."></v-card>'],
   ]
   return componentsReducer(map)
 }
