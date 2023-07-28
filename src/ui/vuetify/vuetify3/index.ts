9a6ca22eabafcc9f5d2b862ae7c98c6fd06720a8
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-07-29 00:04:19 +0800

    chore(vuetify): add chip

--- src/ui/vuetify/vuetify3/index.ts
+++ src/ui/vuetify/vuetify3/index.ts
@@ -1,11 +1,13 @@
 import { componentsReducer, propsReducer } from '../../utils'
 import button from './button.json'
 import card from './card.json'
+import chip from './chip.json'
 
 export function vuetify3() {
   const map: any = [
     button,
     card,
+    chip,
   ]
 
   return propsReducer(map)
@@ -15,6 +17,7 @@ export function vuetify3Components() {
   const map: any[] = [
     ['v-button', '按钮', '<v-btn>Button</v-btn>'],
     ['v-card', '卡片', '<v-card text="..."></v-card>'],
+    ['v-chip', '纸片', '<v-chip>Chip</v-chip'],
   ]
   return componentsReducer(map)
 }
