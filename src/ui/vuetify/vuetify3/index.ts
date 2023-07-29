306160154217dff927ab431188069c32782cf8e1
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-07-29 17:07:11 +0800

    chore(vuetify): add dialog

--- src/ui/vuetify/vuetify3/index.ts
+++ src/ui/vuetify/vuetify3/index.ts
@@ -2,12 +2,14 @@ import { componentsReducer, propsReducer } from '../../utils'
 import button from './button.json'
 import card from './card.json'
 import chip from './chip.json'
+import dialog from './dialog.json'
 
 export function vuetify3() {
   const map: any = [
     button,
     card,
     chip,
+    dialog,
   ]
 
   return propsReducer(map)
@@ -18,6 +20,11 @@ export function vuetify3Components() {
     ['v-button', '按钮', '<v-btn>Button</v-btn>'],
     ['v-card', '卡片', '<v-card text="..."></v-card>'],
     ['v-chip', '纸片', '<v-chip>Chip</v-chip'],
+    ['v-dialog', '对话框', `<v-dialog
+  v-model="dialog"
+  activator="parent"
+  width="auto"
+></v-dialog`],
   ]
   return componentsReducer(map)
 }
