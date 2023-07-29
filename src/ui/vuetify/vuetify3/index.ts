34dda539539603f21e1eddcd826e9bf0c5a4553c
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-07-29 20:51:05 +0800

    chore(vuetify): add expansionPanel expansionPanels expansionPanelText expansionPanelTitle

--- src/ui/vuetify/vuetify3/index.ts
+++ src/ui/vuetify/vuetify3/index.ts
@@ -4,6 +4,10 @@ import card from './card.json'
 import chip from './chip.json'
 import dialog from './dialog.json'
 import divider from './divider.json'
+import expansionPanel from './expansionPanel.json'
+import expansionPanels from './expansionPanels.json'
+import expansionPanelText from './expansionPanelText.json'
+import expansionPanelTitle from './expansionPanelTitle.json'
 
 export function vuetify3() {
   const map: any = [
@@ -12,6 +16,10 @@ export function vuetify3() {
     chip,
     dialog,
     divider,
+    expansionPanel,
+    expansionPanels,
+    expansionPanelText,
+    expansionPanelTitle,
   ]
 
   return propsReducer(map)
@@ -28,6 +36,13 @@ export function vuetify3Components() {
   width="auto"
 ></v-dialog`],
     ['v-divider', '组件用于分隔列表或布局的各个部分', '<v-divider></v-divider>'],
+    ['v-expansion-panels', '扩展面板 组件有助于减少大量信息的垂直空间占用。 组件默认仅显示一个扩展面板；但你可以使用 multiple 属性后，扩展面板可以保持打开，直到主动关闭', '<v-expansion-panels></v-expansion-panels>'],
+    ['v-expansion-panel', '扩展面板 用于包裹 v-expansion-panel-text 和 v-expansion-panel-title 的子组件', `<v-expansion-panel  title="Title"
+  text="Lorem"
+></v-expansion-panel>`],
+    ['v-expansion-title', '高级版扩展面板 用于展示可扩展面板的标题的子组件。 也可以包裹在 #title 插槽内', '<v-expansion-panel-title></v-expansion-panel-title>'],
+    ['v-expansion-text', '高级版扩展面板 用于展示可扩展面板的内容的子组件。 也可以包裹在 #text 插槽内', '<v-expansion-panel-text></v-expansion-panel-text>'],
+
   ]
   return componentsReducer(map)
 }
