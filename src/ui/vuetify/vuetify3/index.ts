05f9ee949cf4af26ebdfc773ed75386ea5cbbb5f
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-07-31 21:48:04 +0800

    chore(vuetify): add listImg listItemAction listItemMedia

--- src/ui/vuetify/vuetify3/index.ts
+++ src/ui/vuetify/vuetify3/index.ts
@@ -14,8 +14,11 @@ import expansionPanelText from './expansionPanelText.json'
 import expansionPanelTitle from './expansionPanelTitle.json'
 import list from './list.json'
 import listItem from './listItem.json'
-import listSubheader from './listSubheader.json'
 import listGroup from './listGroup.json'
+import listImg from './listImg.json'
+import listItemAction from './listItemAction.json'
+import listItemMedia from './listItemMedia.json'
+import listSubheader from './listSubheader.json'
 
 export function vuetify3() {
   const map: any = [
@@ -36,6 +39,9 @@ export function vuetify3() {
     listItem,
     listSubheader,
     listGroup,
+    listImg,
+    listItemAction,
+    listItemMedia,
   ]
 
   return propsReducer(map)
@@ -64,7 +70,14 @@ export function vuetify3Components() {
     ['v-expansion-title', '高级版扩展面板 用于展示可扩展面板的标题的子组件。 也可以包裹在 #title 插槽内', '<v-expansion-panel-title></v-expansion-panel-title>'],
     ['v-expansion-text', '高级版扩展面板 用于展示可扩展面板的内容的子组件。 也可以包裹在 #text 插槽内', '<v-expansion-panel-text></v-expansion-panel-text>'],
     ['v-list', '列表 简单地用于表明某几个元素属于同一个集合。 列表为同组的文字和图像提供了了相同的样式', '<v-list lines="one"></v-list>'],
-
+    ['v-list-group', '列表 可以使用 v-list-group 组件创建列表的子列表。', '<v-list-group></v-list-group>'],
+    ['v-list-img', '列表', '<v-list-img></v-list-img>'],
+    ['v-list-subheader', '列表', '<v-list-subheader></v-list-subheader>'],
+    ['v-list-item', '导航列表', '<v-list-item></v-list-item>'],
+    ['v-list-item-title', '导航列表', '<v-list-item-title></v-list-item-title>'],
+    ['v-list-item-subtitle', '导航列表', '<v-list-item-subtitle></v-list-item-subtitle>'],
+    ['v-list-item-action', '导航列表', '<v-list-item-action></v-list-item-action>'],
+    ['v-list-item-media', '导航列表', '<v-list-item-media></v-list-item-media>'],
   ]
   return componentsReducer(map)
 }
