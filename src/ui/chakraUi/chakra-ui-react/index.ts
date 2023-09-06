6a72d9b9b3f4771fd319b64e9bc4be78a2e01ee0
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-09-06 11:05:16 +0800

    chore(chakra-ui-react): add AvatarBadge AvatarGroup

--- src/ui/chakraUi/chakra-ui-react/index.ts
+++ src/ui/chakraUi/chakra-ui-react/index.ts
@@ -2,12 +2,16 @@ import { componentsReducer, propsReducer } from '../../utils'
 import Accordion from './Accordion.json'
 import Alert from './Alert.json'
 import Avatar from './Avatar.json'
+import AvatarBadge from './AvatarBadge.json'
+import AvatarGroup from './AvatarGroup.json'
 
 export function chakraUiReact2() {
   const map: any = [
     Accordion,
     Alert,
     Avatar,
+    AvatarBadge,
+    AvatarGroup,
   ]
 
   return propsReducer(map)
@@ -17,7 +21,9 @@ export function chakraUiReact2Components() {
   const map = [
     [Accordion.name, '手风琴', '<Accordion></Accordion>'],
     [Alert.name, '警报', '<Alert status=\'error\'></Alert>'],
-    [Avatar.name, '警报', ' <Avatar name=\'Dan Abrahmov\' src=\'https://bit.ly/dan-abramov\' />'],
+    [Avatar.name, '头像', ' <Avatar name=\'Dan Abrahmov\' src=\'https://bit.ly/dan-abramov\' />'],
+    [AvatarBadge.name, '头像徽章', ' <AvatarBadge boxSize=\'1.25em\' bg=\'green.500\' />'],
+    [AvatarGroup.name, '头像组', ' <AvatarGroup spacing=\'1rem\'></AvatarGroup>'],
   ]
   return componentsReducer(map)
 }
