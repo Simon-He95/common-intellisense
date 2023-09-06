3680d1105eeb7182a003911740e5fe1b2e72ae57
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-09-06 15:42:30 +0800

    chore(chakra-ui-react): add Breadcrumb BreadcrumbItem BreadcrumbLink BreadcrumbSeparator

--- src/ui/chakraUi/chakra-ui-react/index.ts
+++ src/ui/chakraUi/chakra-ui-react/index.ts
@@ -4,6 +4,10 @@ import Alert from './Alert.json'
 import Avatar from './Avatar.json'
 import AvatarBadge from './AvatarBadge.json'
 import AvatarGroup from './AvatarGroup.json'
+import Breadcrumb from './Breadcrumb.json'
+import BreadcrumbItem from './BreadcrumbItem.json'
+import BreadcrumbLink from './BreadcrumbLink.json'
+import BreadcrumbSeparator from './BreadcrumbSeparator.json'
 
 export function chakraUiReact2() {
   const map: any = [
@@ -12,6 +16,10 @@ export function chakraUiReact2() {
     Avatar,
     AvatarBadge,
     AvatarGroup,
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbSeparator,
   ]
 
   return propsReducer(map)
@@ -21,9 +29,13 @@ export function chakraUiReact2Components() {
   const map = [
     [Accordion.name, '手风琴', '<Accordion></Accordion>'],
     [Alert.name, '警报', '<Alert status=\'error\'></Alert>'],
-    [Avatar.name, '头像', ' <Avatar name=\'Dan Abrahmov\' src=\'https://bit.ly/dan-abramov\' />'],
-    [AvatarBadge.name, '头像徽章', ' <AvatarBadge boxSize=\'1.25em\' bg=\'green.500\' />'],
-    [AvatarGroup.name, '头像组', ' <AvatarGroup spacing=\'1rem\'></AvatarGroup>'],
+    [Avatar.name, '头像', '<Avatar name=\'Dan Abrahmov\' src=\'https://bit.ly/dan-abramov\' />'],
+    [AvatarBadge.name, '头像徽章', '<AvatarBadge boxSize=\'1.25em\' bg=\'green.500\' />'],
+    [AvatarGroup.name, '头像组', '<AvatarGroup spacing=\'1rem\'></AvatarGroup>'],
+    [Breadcrumb.name, '面包屑', '<Breadcrumb></Breadcrumb>'],
+    [BreadcrumbItem.name, '面包屑项', '<BreadcrumbItem></BreadcrumbItem>'],
+    [BreadcrumbLink.name, '面包屑链接', '<BreadcrumbLink></BreadcrumbLink>'],
+    [BreadcrumbSeparator.name, '每个面包屑之间的视觉分隔符', '<BreadcrumbSeparator />'],
   ]
   return componentsReducer(map)
 }
