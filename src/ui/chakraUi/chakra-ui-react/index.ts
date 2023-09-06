ae9b88e6cdac4b5bf39784e8ec2afe6c3e5026f3
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-09-06 10:59:57 +0800

    chore(chakra-ui-react): add Avatar

--- src/ui/chakraUi/chakra-ui-react/index.ts
+++ src/ui/chakraUi/chakra-ui-react/index.ts
@@ -1,11 +1,13 @@
 import { componentsReducer, propsReducer } from '../../utils'
 import Accordion from './Accordion.json'
 import Alert from './Alert.json'
+import Avatar from './Avatar.json'
 
 export function chakraUiReact2() {
   const map: any = [
     Accordion,
-    Alert
+    Alert,
+    Avatar,
   ]
 
   return propsReducer(map)
@@ -15,6 +17,7 @@ export function chakraUiReact2Components() {
   const map = [
     [Accordion.name, '手风琴', '<Accordion></Accordion>'],
     [Alert.name, '警报', '<Alert status=\'error\'></Alert>'],
+    [Avatar.name, '警报', ' <Avatar name=\'Dan Abrahmov\' src=\'https://bit.ly/dan-abramov\' />'],
   ]
   return componentsReducer(map)
 }
